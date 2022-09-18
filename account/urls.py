9e4c828452370265e09c772ddd4c5054094af875
SujayD123 <sujaydammoju@gmail.com>
2022-09-18 16:05:57 -0400

    code

---
+++ account/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from . import views
+from django.contrib.auth import views as auth_views
+
+app_name = 'account'
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(redirect_authenticated_user=True)),
+    path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile)
+]
---
+++ account/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from .forms import RegistrationForm
+from django.contrib import messages
+from django.contrib.auth import login
+from django.conf import settings
+
+def register(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration Successful.")
+            return redirect(settings.LOGIN_REDIRECT_URL)
+        else:
+            print(form)
+            print("INvlaie fgorme.")
+            print(form.errors)
+            return render(request, 'registration/register.html', {'form':form})
+    form = RegistrationForm()
+    return render(request = request, template_name = "registration/register.html", context={"form": form})
+
+def home(request):
+    return render(request, "home.html", {})
+
+def profile(request):
+    return render(request, "profile.html", {})
